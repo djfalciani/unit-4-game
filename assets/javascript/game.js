1d55ad8650fc63510c335079b77d477372d7ac70
Danny Falciani <Daniel@danny-falcianis-macbook-pro.local>
2019-06-08 21:35:10 -0400

    added basic elements and avatar objects

---
+++ assets/javascript/game.js
@@ -0,0 +1,31 @@
+// Globabl Variables
+avatar1 = {
+    HP  : 100,
+    AP  : 5,
+    CAP : 6
+};
+
+avatar2 = {
+    HP  : 120,
+    AP  : 6,
+    CAP : 5
+};
+
+avatar3 = {
+    HP  :   150,
+    AP  :   8,
+    CAP :   9
+};
+
+avatar4 = {
+    HP  :   200,
+    AP  :   10,
+    CAP :   10
+};
+
+var playerHP = 0;
+var advisaryHP = 0;
+var attackCounter = 0;
+var playMode = false;
+
+// When User 
